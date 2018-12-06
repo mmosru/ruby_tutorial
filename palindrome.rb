1f6687a777f7307ac258431e153e24a015f67ae8
mmosru <oconnor@my.ccsu.edu>
2018-12-06 16:17:34 +0000

    Finish Ch. 7

--- palindrome.rb
+++ palindrome.rb
@@ -1,5 +1,19 @@
-# Returns true for a palindrom, false otherwise.
-def palindrome?(string)
-  processed_content = string.downcase
-  processed_content == processed_content.reverse
+module Palindrome
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+  
+  # Returns true if String is blank, false otherwise.
+  def blank?
+    !!processed_content.match(/\A(^\s+$)*\z/)
+  end
+  
+  private
+  
+    # Returns content for palindrome testing.
+    def processed_content
+      self.to_s.downcase
+    end
 end
